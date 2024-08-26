9e18927574e317794ebe03cd6838a742adbe90d4
HipfunnyKid <115962413+HipfunnyKid@users.noreply.github.com>
2024-08-26 11:51:06 +1000

    -first commit

---
+++ src/main/java/com/scholarsmc/voxelrunners/BookModDataGenerator.java
@@ -0,0 +1,11 @@
+package com.scholarsmc.voxelrunners;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BookModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
