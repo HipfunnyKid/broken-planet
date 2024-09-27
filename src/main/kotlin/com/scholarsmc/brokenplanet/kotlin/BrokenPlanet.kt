9f12e7b81fb2d85036bc6e42fd3c7fa21e2152d2
HipfunnyKid <115962413+HipfunnyKid@users.noreply.github.com>
2024-09-27 14:06:35 +1000

    Optimised Code

--- src/main/kotlin/com/scholarsmc/brokenplanet/kotlin/BrokenPlanet.kt
+++ src/main/kotlin/com/scholarsmc/brokenplanet/kotlin/BrokenPlanet.kt
@@ -6,6 +6,7 @@ object BrokenPlanet : ModInitializer {
     private val logger = LoggerFactory.getLogger("brokenplanet")
 
 	override fun onInitialize() {
+        logger.info("Initializing Broken Planet Kotlin Classes")
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
